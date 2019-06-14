01c965d996144992737d24448c3e2f8cd89ee946
mika1407 <koneautomaatio@hotmail.com>
2019-06-15 02:39:25 +0300

    MADLIB GENERATOR added

--- JSTut.js
+++ JSTut.js
@@ -30,3 +30,53 @@ function fib(whichNum){
     }
     return num2;
 }
+
+ // ----- MADLIB GENERATOR -----
+ 
+// Create the MadLib using a ~ every place we want to 
+// enter a user supplied string
+let mLText = "My dear old ~ sat me down to hear some words of wisdom \n 1. Give a man a ~ and you ~ him for a day ~ a man to ~ and he'll ~ forever \n 2. He who ~ at the right time can ~ again \n 3. Always wear ~ ~ in case you're in a ~ \n 4. Don't use your ~ to wipe your ~ Always have a clean ~ with you";
+
+ // Convert the string into an array where spaces determine
+// the individual pieces to put in each array cell
+let mLArray = mLText.split(" ");
+ 
+// Array that will hold user input
+let inputArray = [];
+ 
+function madLibGenerator(){
+    createInputArray();
+
+    if(checkForMissingInput()){
+        document.getElementById("output1").value = "Enter all values above";
+    } else {
+        createMLSentence();
+    }
+}
+ 
+function createInputArray(){
+    for(i = 0; i <= 13; i++){
+        inputArray[i] = document.getElementById("i" + i).value;
+    }
+}
+ 
+function checkForMissingInput(){
+    let defaultArrayVals = ["Person", "Noun", "Verb", "Adjective", "Plural Verb", "Body Part", "Event"];
+
+    for(i=0; i < inputArray.length; i++){
+        if(defaultArrayVals.indexOf(inputArray[i]) > -1 ){
+            return true;
+        }
+    }
+    return false;
+}
+ 
+function createMLSentence(){
+    let arrIndex = 0;
+    for(i=0; i < mLArray.length; i++){
+        let matchIndex = mLArray.indexOf("~");
+        mLArray[matchIndex] = inputArray[arrIndex];
+        arrIndex++;
+    }
+    document.getElementById("output1").value = mLArray.join(" ");
+}
