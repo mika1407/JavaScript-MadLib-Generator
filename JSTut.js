0bebc6d9d1446d4d06361aca231c3ea8569df4ca
mika1407 <koneautomaatio@hotmail.com>
2019-06-15 01:18:23 +0300

    pi+fib

---
+++ JSTut.js
@@ -0,0 +1,32 @@
+// Pi = 4/1 - 4/3 + 4/5 - 4/7...
+
+function calcPI(iterations){
+    let pi =0, divisor = 1;
+    for(i = 0; i <= iterations; i++){
+        pi = pi + (4/divisor) - (4/(divisor + 2));
+        divisor += 4;
+    }
+    document.getElementById("output1").value = pi.toFixed(10);
+}
+
+// 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, ...
+
+let fibList = [];
+function getFibList(howMany){
+    for(i = 0; i < howMany; i++){
+        fibList[i] = fib(i);
+    }
+    fibList.shift();
+    document.getElementById("output1").value = fibList.join(", ");
+}
+
+function fib(whichNum){
+    let num1 = 1, num2 = 0, temp, i=0;
+    while(i < whichNum){
+        temp = num1;
+        num1 = num1 + num2;
+        num2 = temp;
+        i++;
+    }
+    return num2;
+}
